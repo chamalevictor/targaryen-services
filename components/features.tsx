2d3a1021d49e7f786647d488f57533f95efe279f
chamalevictor <chamale.victor@gmail.com>
2023-10-24 23:55:01 -0600

    feat: modificaciones

---
+++ components/features.tsx
@@ -0,0 +1,158 @@
+export default function Features() {
+  return (
+    <section>
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="py-12 md:py-20">
+          {/* Section header */}
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+            <h2 className="h2 mb-4">
+              Clientes que aprecian la autenticidad de nuestros productos
+              únicos.
+            </h2>
+          </div>
+
+          {/* Items */}
+          <div
+            className="max-w-sm mx-auto grid gap-8 md:grid-cols-2 lg:grid-cols-3 lg:gap-16 items-start md:max-w-2xl lg:max-w-none"
+            data-aos-id-blocks
+          >
+            {/* 1st item */}
+            <div
+              className="relative flex flex-col items-center"
+              data-aos="fade-up"
+              data-aos-anchor="[data-aos-id-blocks]"
+            >
+              <svg
+                className="w-16 h-16 mb-4"
+                viewBox="0 0 64 64"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect
+                  className="fill-current text-red-600"
+                  width="64"
+                  height="64"
+                  rx="32"
+                />
+                <path
+                  className="stroke-current text-red-100"
+                  d="M30 39.313l-4.18 2.197L27 34.628l-5-4.874 6.91-1.004L32 22.49l3.09 6.26L42 29.754l-3 2.924"
+                  strokeLinecap="square"
+                  strokeWidth="2"
+                  fill="none"
+                  fillRule="evenodd"
+                />
+                <path
+                  className="stroke-current text-red-300"
+                  d="M43 42h-9M43 37h-9"
+                  strokeLinecap="square"
+                  strokeWidth="2"
+                />
+              </svg>
+              <h4 className="h4 mb-2">Calidad</h4>
+              <p className="text-lg text-gray-400 text-center">
+                Nos enorgullece ofrecer productos de calidad excepcional con
+                materiales de primera. Cada producto es meticulosamente diseñado
+                para satisfacer las más altas expectativas.
+              </p>
+            </div>
+
+            {/* 5th item */}
+            <div
+              className="relative flex flex-col items-center"
+              data-aos="fade-up"
+              data-aos-delay="400"
+              data-aos-anchor="[data-aos-id-blocks]"
+            >
+              <svg
+                className="w-16 h-16 mb-4"
+                viewBox="0 0 64 64"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect
+                  className="fill-current text-red-600"
+                  width="64"
+                  height="64"
+                  rx="32"
+                />
+                <g
+                  strokeLinecap="square"
+                  strokeWidth="2"
+                  fill="none"
+                  fillRule="evenodd"
+                >
+                  <path
+                    className="stroke-current text-red-100"
+                    d="M29 42h10.229a2 2 0 001.912-1.412l2.769-9A2 2 0 0042 29h-7v-4c0-2.373-1.251-3.494-2.764-3.86a1.006 1.006 0 00-1.236.979V26l-5 6"
+                  />
+                  <path
+                    className="stroke-current text-red-300"
+                    d="M22 30h4v12h-4z"
+                  />
+                </g>
+              </svg>
+              <h4 className="h4 mb-2">Aceptación</h4>
+              <p className="text-lg text-gray-400 text-center">
+                Hemos experimentado una increíble aceptación en el mercado,
+                respaldada por la satisfacción constante de nuestros clientes.
+                Su confianza en nuestros productos es nuestro mayor logro.
+              </p>
+            </div>
+
+            {/* 6th item */}
+            {/* 3rd item */}
+            <div
+              className="relative flex flex-col items-center"
+              data-aos="fade-up"
+              data-aos-delay="200"
+              data-aos-anchor="[data-aos-id-blocks]"
+            >
+              <svg
+                className="w-16 h-16 mb-4"
+                viewBox="0 0 64 64"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect
+                  className="fill-current text-red-600"
+                  width="64"
+                  height="64"
+                  rx="32"
+                />
+                <g
+                  transform="translate(21 21)"
+                  strokeLinecap="square"
+                  strokeWidth="2"
+                  fill="none"
+                  fillRule="evenodd"
+                >
+                  <ellipse
+                    className="stroke-current text-red-300"
+                    cx="11"
+                    cy="11"
+                    rx="5.5"
+                    ry="11"
+                  />
+                  <path
+                    className="stroke-current text-red-100"
+                    d="M11 0v22M0 11h22"
+                  />
+                  <circle
+                    className="stroke-current text-red-100"
+                    cx="11"
+                    cy="11"
+                    r="11"
+                  />
+                </g>
+              </svg>
+              <h4 className="h4 mb-2">Disponibilidad</h4>
+              <p className="text-lg text-gray-400 text-center">
+                Nuestros productos están disponibles de manera conveniente para
+                que puedas acceder a ellos fácilmente cuando lo desees. Tu
+                satisfacción está a solo un clic de distancia.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
